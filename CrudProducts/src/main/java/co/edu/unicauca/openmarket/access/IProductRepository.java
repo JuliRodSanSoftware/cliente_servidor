adf78a6cd79f56033eaee37dcdfffc7a8aa35470
Rodrigo A Guspian <rodalejo8@gmail.com>
2023-05-14 11:25:31 -0500

    Añadiendo el proyecto al repositorio

---
+++ CrudProducts/src/main/java/co/edu/unicauca/openmarket/access/IProductRepository.java
@@ -0,0 +1,21 @@
+
+package co.edu.unicauca.openmarket.access;
+
+import co.edu.unicauca.openmarket.domain.Product;
+import java.util.List;
+
+/**
+ *
+ * @author Libardo, Julio
+ */
+public interface IProductRepository {
+    boolean save(Product newProduct);
+    
+    boolean edit(Long id, Product product);
+    
+    boolean delete(Long id);
+
+    Product findById(Long id);
+    
+    List<Product> findAll();
+}
